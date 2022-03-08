b9646e6e29c69383ee34fa82fb967b270bcd594c
boubecar <72895476+boubecar@users.noreply.github.com>
2022-03-08 11:34:41 +0100

    Add files via upload

---
+++ Commands/PolinaDBContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+//using WebApiCqrs.Models;
+
+namespace CommonData
+{
+    public class PolinaDBContext : DbContext
+    {
+        public PolinaDBContext() {
+            }
+        public PolinaDBContext(DbContextOptions<PolinaDBContext> options)  
+          : base(options)
+        {
+        }
+
+       public DbSet<Filiale> fliales { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {    if(!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Data Source=.;Initial Catalog = PolinaDbv1;Integrated Security=true");
+            }
+            base.OnConfiguring(optionsBuilder);
+        }
+
+    }
+}
